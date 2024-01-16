1f60c1fe4e456ba449b0a363dec85c6b7a6398df
cool guy #34 <123191180+Mrsagi12@users.noreply.github.com>
2024-01-16 14:30:06 +0200

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void f1(String name)
+    {
+        System.out.println("Hello "+name);
+    }
+    
+    
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter your name: ");
+		String name = scan.nextLine();
+		
+		f1(name);
+	}
+}
